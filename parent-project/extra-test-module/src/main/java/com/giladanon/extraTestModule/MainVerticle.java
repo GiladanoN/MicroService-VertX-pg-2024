1da18453542e125d7c1399a0103ade4340ba4e73
GiladanoN <giladanon@gmail.com>
2024-04-13 23:45:21 +0300

    added filesystem impl (file.json reading/saving)
    works currently by adding order without checking anything about the order itself

--- parent-project/extra-test-module/src/main/java/com/giladanon/extraTestModule/MainVerticle.java
+++ parent-project/extra-test-module/src/main/java/com/giladanon/extraTestModule/MainVerticle.java
@@ -1,11 +1,28 @@
 package com.giladanon.extraTestModule;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.AsyncResult;
 // import io.vertx.core.AsyncResult;
 // import io.vertx.core.Future;
 // import io.vertx.core.Handler;
 import io.vertx.core.Promise;
+import io.vertx.core.buffer.Buffer;
 import io.vertx.core.eventbus.Message;
+import io.vertx.core.file.AsyncFile;
+import io.vertx.core.file.OpenOptions;
+import io.vertx.core.json.Json;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Objects;
+
+import com.giladanon.sharedModule.Common.Codecs.GenericJsonCodec;
+import com.giladanon.sharedModule.Common.Exceptions.ValidationError;
+import com.giladanon.sharedModule.Common.POJO.Order;
+import com.giladanon.sharedModule.Common.POJO.OrderToAdd;
+// import com.hazelcast.internal.json.Json;
 
 public class MainVerticle extends AbstractVerticle {
 
@@ -14,21 +31,56 @@ public class MainVerticle extends AbstractVerticle {
 
     // vertx.eventBus().registerDefaultCodec(OrderToAdd.class, new OrderToAddCodec());
     vertx.eventBus().registerDefaultCodec(
-        OrderToAdd.class, new GenericCodec<OrderToAdd>(OrderToAdd.class)
+        OrderToAdd.class, new GenericJsonCodec<OrderToAdd>(OrderToAdd.class)
       );
 
     // this.vertx.fileSystem()
-    vertx.eventBus().consumer("ADD_ORDER", (Message<OrderToAdd> message) -> {
+    vertx.eventBus().consumer("ADD_ORDER", (Message<OrderToAdd> message) ->
+    {
       System.out.println("ADD_ORDER consumer was called over the eventbus"); 
-      OrderToAdd data = message.body();
-      String asString = (data == null ? "NONE" : data.toString());
-      System.out.println("data recieved: " + asString);
+      OrderToAdd data = extractData(message);
+
+      OrderAdder adder = new OrderAdder(data, vertx.fileSystem());
+      
+      try {
+        adder.validate();
+        // adder.openOrCreateFile();
+        // adder.readAndParseOrders();
+        // adder.addOrderToList();
+        // adder.dumpListBackToFile();
+        // Path path = adder.getFullFilePath().toRealPath();
+        // String pathStr = (path != null ? path.toString() : "UNCLEAR");
+        // System.out.println("writing to file has completed! file: " + pathStr);
+        adder.addOrderProcedure()
+          .andThen(x ->
+            System.out.println("Goodbye from last 'andThen' -- adder.addOrderProcedure()"));
+        
+        // System.out.println("Called adder.addOrderProcedure()");
+      }
+      catch (
+        ValidationError |   // data recieved found to be invalid.
+        IOException e      // something went wrong handling the filesystem (dir / file etc.)
+      ) {
+        System.out.println("Cannot complete ADD_ORDER request. returning early...");
+        message.reply(false);  // TODO- add reply mechanism to requester
+        return;        
+      }
+      
+      // printPWD();  // Debug
+
+      // Path path = adder.getFullFilePath();
+      // String pathStr = (path != null ? path.toString() : "UNCLEAR");
+      // System.out.println("writing to file handler is registered, file: " + pathStr);
+
     })
     // .handler(null)
     .completionHandler( (v) -> {
         System.out.println("Registered consumer ADD_ORDER successfully = " + v.succeeded());
+        System.out.println(); // newline
       }
     );
+
+    
 
     // final Router router = Router.router(vertx);
     
@@ -40,6 +92,25 @@ public class MainVerticle extends AbstractVerticle {
     System.out.println("Strated OrderVerticle");
     startPromise.complete();
 
+  }
+
+  private OrderToAdd extractData(Message<OrderToAdd> message) {
+    OrderToAdd data = message.body();
+    String asString = (data == null ? "NONE" : data.toString());
+    System.out.println("data recieved: " + asString);
+    return data;
+  }
+
+  private void printPWD() {
+    try {
+      // vertx.fileSystem().
+
+    String currentPath = new java.io.File(".").getCanonicalPath();
+    System.out.println("Current dir:" + currentPath);
+   
+    String currentDir = System.getProperty("user.dir");
+    System.out.println("Current dir using System:" + currentDir);
+    } catch (Exception e) {}
   }
 
 }
---
+++ parent-project/extra-test-module/src/main/java/com/giladanon/extraTestModule/OrderAdder.java
@@ -0,0 +1,395 @@
+package com.giladanon.extraTestModule;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.function.Function;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import com.giladanon.sharedModule.Common.Exceptions.ValidationError;
+import com.giladanon.sharedModule.Common.POJO.Order;
+import com.giladanon.sharedModule.Common.POJO.OrderList;
+import com.giladanon.sharedModule.Common.POJO.OrderToAdd;
+
+import io.vertx.core.AsyncResult;
+import io.vertx.core.Future;
+import io.vertx.core.Promise;
+import io.vertx.core.buffer.Buffer;
+import io.vertx.core.file.AsyncFile;
+import io.vertx.core.file.FileSystem;
+import io.vertx.core.file.OpenOptions;
+import io.vertx.core.impl.Arguments;
+import io.vertx.core.json.Json;
+
+public class OrderAdder {
+
+  private OrderToAdd data;
+  private FileSystem fileSystem;
+
+  private Order order; // = data.getOrderToAdd();
+  private String username; // = data.getUser();
+  private FileHandler fileHandler;
+
+  private OrderList updatedList;
+  
+  public OrderAdder(OrderToAdd data, FileSystem fs) {
+    this.data = data;
+    this.fileSystem = fs;
+  }
+
+  public void validate() throws ValidationError {  // gaurd-clause style (cascading pre-requisites)
+
+    if (data == null)
+      throw new ValidationError("data object provided is null (missing data).");
+
+    this.order = data.getOrderToAdd();
+    this.username = data.getUser();
+
+    if (order == null || username == null)
+      throw new ValidationError("some required data-field provided is null (missing).");
+
+    final boolean doSubValidation = false; // remove once implemented
+    if (doSubValidation) {
+      // !toAdd.valid || userStr.isEmpty()
+      // TODO- add fields validation on order object
+      throw new ValidationError(
+        "some required sub-field(s) failed upon relevnt validation.");
+    }
+
+    if (fileSystem == null)
+      throw new ValidationError(
+        "fileSystem object is required, provided is null (cannot operate files).");
+
+    // System.out.println("everything checks out, state is valid!"); // DEBUG
+  }
+
+  public Future<OrderList> addOrderProcedure() throws IOException {
+
+    System.out.println("Starting the addOrderProcedure() .");
+
+    AsyncFile openedFile;
+    
+    // Future<AsyncFile> openFileFuture = this.openOrCreateFile();
+    Future<OrderList> procFuture =
+    this.openOrCreateFile()
+      // .map((AsyncFile file) -> fileHandler.readFileContents(file) )
+      // .map((Future<Buffer> future) -> future.result())
+      // .andThen( (AsyncResult<AsyncFile> fileRes) -> { openedFile = fileRes.result(); } )
+      // .map( (AsyncFile file) -> fileHandler.readFileContents(file).result() )
+      // .map( (AsyncFile file) -> {
+      //   System.out.println("asyncFile: >>> " + file);
+      //   Future<Buffer> bf =
+      //   fileHandler.readFileContents(file);
+      //   Buffer b = bf.result();
+      //   System.out.println("bf: >>> " + bf);
+      //   System.out.println("b: >>> " + b);
+      //   return (
+      //     bf.onComplete(res -> {
+      //       System.out.println("res.success: >>> " + res.succeeded());
+      //       System.out.println("res.result: >>> " + res.result());
+      //       System.out.println("res.cause: >>> " + res.cause());          
+      //     }).result()
+      //   );
+      //   // return b;
+      // } )
+      .map( (AsyncFile file) -> fileHandler.readFileContents(file).onComplete(r->{}) )
+      .compose( (Future<Buffer> fb) -> fb )
+      .map( (Buffer contents) ->  fileHandler.parseOrdersJsonArray(contents).result() )
+      .map( (OrderList list) -> this.addOrderToList(list, this.order) ) // produces the updated list
+      .andThen( (AsyncResult<OrderList> res) -> {
+        // if (res.failed()) {
+        //   System.out.println("HERE HERE 83");
+        //   res.cause().printStackTrace();
+        //   Throwable temp = new Throwable();
+        //   temp.setStackTrace(
+        //     (StackTraceElement[])
+        //     Arrays.stream(res.cause().getStackTrace())
+        //     .filter(x -> x.getClassName().contains("giladanon"))
+        //     .collect(Collectors.toList()).toArray()
+        //   );
+        //   temp.printStackTrace();
+        // }
+        fileHandler.writeContentsToFile( res.result() );  // dump list back to file
+      })
+      .andThen( ignore -> {
+        Path path = this.getFullFilePath(); //.toRealPath();
+        String pathStr = (path != null ? path.toString() : "UNCLEAR");
+        System.out.println("writing to file has completed! file: " + pathStr);    
+      }) ;
+
+
+    System.out.println("Successfuly completed addOrderProcedure() !");
+    return procFuture;
+
+  }
+
+  public Future<AsyncFile> openOrCreateFile() throws IOException {
+
+    if (username == null)  // shouldn't happen if validate ran successfully
+      username = data.getUser();
+    
+    if (fileHandler == null)  // lazy init
+      this.fileHandler = new FileHandler(username);
+    
+    Path path = fileHandler.ensureFilePath();
+    return fileHandler.openDataFile(fileHandler.fullFilePath);
+  }
+
+  // public void readAndParseOrders() {
+  //   fileHandler.readFileContents();
+  //   fileHandler.parseOrdersJsonArray();
+  // }
+
+  public OrderList addOrderToList(OrderList list, Order toAdd) {
+    if (list == null) {
+      String err = "An OrderList object is not provided - no container to update...";
+      throw new NullPointerException(err);
+    }
+    if (this.order == null)
+      System.out.println("An order object is not provided - nothing to add...");
+    else {
+      list.getOrdersList().add(order);  // add item to the list
+    }
+
+    this.updatedList = list;
+    return list;
+  }
+
+  public void dumpListBackToFile() {
+    fileHandler.writeContentsToFile(this.updatedList);
+  }
+
+  public Path getFullFilePath() {
+    if (fileHandler == null) return null;
+    return fileHandler.getFullFilePath();
+  }
+
+  private class FileHandler {
+
+    private final Path folderPath =
+      Paths.get(".", "data", "orders");  // hardcoded
+    
+    private final OpenOptions options =
+      new OpenOptions()
+        .setCreate(true)  // create file if missing, without error if already exists
+        .setTruncateExisting(false) // ensure contents are preserved (if present)
+        // .setAppend(true)  // open writing in append-mode (assumes file already has contents)
+        .setRead(true)
+        .setWrite(true);  // allow read AND write ops.
+    
+    private String filename;
+    private Path fullFilePath;
+
+    private Future<AsyncFile> openFile;
+    // private Future<Buffer> readFile;
+    // private Future<OrderList> parsedList;
+    // private AsyncFile asyncFile;
+
+    public FileHandler(String filename) {
+      this.filename = filename + ".json";
+      this.fullFilePath =
+        Paths.get(folderPath.toString(), this.filename);
+    }
+
+    public Path ensureFilePath() throws IOException {
+
+      System.out.println("Running --- ensureFilePath()");
+      // create directories to allow file creation / update
+      try {
+        return Files.createDirectories(folderPath);
+      }
+      catch (IOException e) {
+        System.out.println(
+          "ERORR :: Skipping file opening/writing, direcrtor(ies) could not be created.\n" +
+          "relevant path: " + folderPath
+        );
+        e.printStackTrace();
+        throw e;  // propagate the error once printed above.
+      }
+  
+    }
+  
+    public Future<AsyncFile> openDataFile(Path path) {
+      System.out.println("Running --- openDataFile()");
+      this.openFile =
+        fileSystem
+        .open(path.toString(), options)
+        .andThen(res -> {
+          String msg = (res.failed()) ?
+            "A problem occured when opening the file" : "Success in opening/creating the file";
+          System.out.println(msg + ", path: " + path);
+          // if (res.succeeded())
+          //   this.asyncFile = res.result();
+        }
+      );
+      return openFile;
+    }
+
+    public Future<Buffer> readFileContents(AsyncFile openFile) {
+
+      System.out.println("Running --- readFileContents()");
+
+      // long readLength = openFile.size().onFailure(
+      //   f -> { System.out.println("Cannot determine file size"); f.printStackTrace(); }
+      // ).result();
+      long readLength = openFile.sizeBlocking();  // TODO
+      System.out.println("got readLen = " + readLength);
+      Buffer readBuffer = Buffer.buffer((int)readLength);
+      System.out.println("> File size to read detected as: (in bytes) " + readLength);
+      warnIfFileTooBig(readLength);
+
+      // openFile.setReadPos(0); // read from file start pos
+      System.out.println("> called 'setReadPos'");
+
+      // System.out.println("Exiting --- readFileContents() via 'readFile(path)'");
+      // if (true)
+      //   return fileSystem.readFile(fullFilePath.toString());
+      
+      Future<Buffer> futureBuffer =
+        openFile.read(readBuffer, 0, 0, (int)readLength)
+        // .onComplete(result ->
+        // {
+        //   if (result.succeeded()) {
+        //     Buffer contentBuffer = result.result();
+        //     System.out.println("Success in reading file contents, len=" + contentBuffer.length());
+        //   }
+        //   else {
+        //     System.out.println("A problem occured when reading a file, path: " + filename);
+        //   }
+        //   // return result;
+        // })
+        ;
+
+      System.out.println("> called 'read' function");
+
+      futureBuffer.result();
+      System.out.println("> called 'result' function");
+      
+      futureBuffer.onFailure(
+        f -> { System.out.println("futureBuffer returned with error.."); f.printStackTrace(); }
+      );
+      System.out.println("> called 'onfailure' function");
+
+      // Future<Buffer> readFileFuture = openFile.map(
+      //   (Function<AsyncFile,Buffer>)(AsyncFile openResult) -> {
+      //     Future<Buffer> contentFuture =
+      //     fileSystem
+      //       .readFile(fullFilePath.toString())
+      //       .andThen(res -> {
+      //         if (res.succeeded()) {
+      //           Buffer buffer = res.result();
+      //           System.out.println(
+      //             "Success in reading file contents (len=" + buffer.length() +"), path: " + filename);
+      //         }
+      //         else
+      //           System.out.println("A problem occured when reading a file, path: " + filename);
+      //       }
+      //     );
+      //       System.out.println("Running --- readFile.andThen registered");
+      //       System.out.println("this.readFile => " + readFile);
+      //     return contentFuture.result();
+      //   }
+      // )
+      // .onFailure(failure -> {
+      //   System.out.println("A problem occured BEFORE file could be read, path: " + filename);
+      //   failure.printStackTrace();
+      // });
+      // return readFileFuture;
+
+      System.out.println("Exiting --- readFileContents()");
+      return futureBuffer;
+    }
+
+    public Future<OrderList> parseOrdersJsonArray(Buffer bufferArg) {
+      System.out.println("Running --- parseOrdersJsonArray()");
+
+      if (bufferArg == null) {
+        System.out.println("Running --- bufferArg provided is null...");
+        Arguments.require(false, "bufferArg provided is null");
+        // bufferArg = readFileContents().result();
+      }
+
+      Promise<OrderList> promise = Promise.<OrderList>promise();
+      Future<OrderList> parsedList = promise.future();
+
+      Buffer readBuffer = bufferArg;
+      
+      if (readBuffer.length() == 0) {
+        System.out.println("nothing found to parse in given file, path: " + filename);
+        promise.complete(new OrderList());  // resolve with empty list (= empty file)
+      }
+      else {  // file has contents
+        System.out.println("trying to parse the given file as OrderList.");
+        try {
+          OrderList list = readBuffer.toJsonObject().mapTo(OrderList.class);
+          promise.complete(list);
+          System.out.println("parsing completed successfylly. list: " + list);
+        }
+        catch (Exception e) {
+          System.out.println("an error was thrown during parsing.");
+          e.printStackTrace();
+          promise.fail(e);
+        }
+      }
+      //   },
+      //   (Throwable failure) -> {
+      //     System.out.println("A problem occured BEFORE file could be parsed, path: " + filename);
+      //     System.out.println(failure.getLocalizedMessage());
+      //     return;
+      //   }
+      // )
+      ;
+
+      return parsedList;
+    }
+
+    // public Future<OrderList> getCurrentOrdersList() {
+    //   System.out.println("Running --- getCurrentOrdersList");
+    //   if (this.parsedList == null) return parseOrdersJsonArray(this.readFile);
+    //   return this.parsedList;
+    // }
+
+    public void writeContentsToFile(OrderList list) {
+      System.out.println("Running --- writeContentsToFile()");
+  
+      if (list == null) {
+        System.out.println("Nothing provided to update the file with... replaceing with empty list");
+        list = new OrderList();
+        list.setOrdersList(Collections.emptyList());
+      }
+      Buffer buffer = Buffer.buffer("");
+      try {
+        String output = Json.encodePrettily(list);
+        buffer.appendString(output);
+        AsyncFile asyncFile = this.openFile.result();
+        asyncFile.setWritePos(0).end(buffer);  // pottential "junk at end" issue, needs checking
+      }
+      catch (Exception e) {
+        System.out.println("Something went wrong preparing or writing the data out...");
+        e.printStackTrace();
+        throw e;
+      }
+      
+    }
+
+    public Path getFullFilePath() {
+      return fullFilePath;
+    }
+
+  }
+
+  private static void warnIfFileTooBig(long fileLength) {
+    if (fileLength > Integer.MAX_VALUE) {
+      System.out.println(
+        "WARNING :: possible issue with buffer size limitation, file size larger than " +
+          + Integer.MAX_VALUE + " bytes (Integer.MAX_VALUE) -- value: " + fileLength);
+    }
+  }
+  
+}
