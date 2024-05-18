ec68141d197eed74d2f259abaa2bd848a6e95ded
GiladanoN <giladanon@gmail.com>
2024-05-18 20:59:33 +0300

    added another component
    refactored out of app-component.
    also moved some css around to appropriate files.

--- angular-project/my-first-app/src/app/app.module.ts
+++ angular-project/my-first-app/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { AngularLogoComponent } from './angular-logo/angular-logo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AngularLogoComponent
   ],
   imports: [
     BrowserModule
